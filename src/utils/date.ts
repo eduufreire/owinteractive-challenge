3a7550f4b98ecbb03965ee12a1c0458b8a40d1ea
eduufreire <eduardo.freire@sptech.school>
2025-01-19 22:32:05 -0300

    route create and date parse

---
+++ src/utils/date.ts
@@ -0,0 +1,18 @@
+import dayjs from "dayjs"
+import utc from "dayjs/plugin/utc"
+
+dayjs.extend(utc)
+
+export class DateParse {
+    private constructor() {}
+    
+    static getDateNow(): Date {
+        const now = dayjs().format("YYYY-MM-DDTHH:mm:ss+00:00")
+        return new Date(now)
+    }
+
+    static formatDate(date: Date, time = true): string {
+        const templateFormat = "YYYY-MM-DD HH:mm:ss"
+        return dayjs(date).utc().format(time ? templateFormat : "YYYY-MM-DD")
+    }
+}
